036e0e20ea3fb65f1c1e4a59da245e3e41f2ad64
Pietro Gagliardi <pietro10@mac.com>
2018-07-28 22:12:59 -0400

    Converted the schemas to Go.

---
+++ schemas.go
@@ -0,0 +1,77 @@
+// 28 july 2018
+package mdlist
+
+import (
+	"golang.org/x/crypto/blake2b"
+	"github.com/google/uuid"
+)
+
+struct Game {
+	GUID		uuid.UUID
+	DisplayName	string		// ASCII only
+}
+
+type Region int
+const (
+	Japan Region = iota
+	US
+	Europe
+	Brazil
+	SouthKorea
+	Australia
+	AsiaNTSC
+	AsiaPAL
+)
+
+type Release struct {
+	GUID				uuid.UUID
+	Game				*Game
+	Region				Region
+	Name				string
+	NameRomanized		string	// Japan, Korea, Asia only
+	NameSource			string
+	Publisher				string
+	ReleaseDate			time.Time
+	ReleaseDateAccuracy	time.Time		// each component: 1 in the most precise known digit (so 199?-??-?? means a ReleaseDate of 1990-1-1 and Accuracy of 10-0-0)
+	ReleaseDateSource		string		// if unspecified (empty), marked as unconfirmed; accuracy should be to the year from the title screen of the game
+	SerialNumber			string
+	// TODO details for variant releases like Sega Classics? if so the asset map will need to change to allow a single ROM to map to multiple releases
+	// TODO box type for arranging image assets?
+}
+
+type Asset struct {
+	GUID		uuid.UUID
+	Size			int64
+	MIME		string
+	Blake2b512	[blake2b.Size]byte
+	Source		string
+	BadReason	string	// if unspecified, asset is considered "good"
+}
+
+type AssetType int
+const (
+	Other AssetType = iota
+	ROM
+	Cover
+	Spine
+	SpineTop
+	SpineRight
+	SpineBottom
+	BackCover
+	Media
+	MediaLeft
+	MediaTop
+	MediaRight
+	MediaBottom
+	MediaBack
+	PCB
+	PCBBack
+	Manual
+)
+
+type AssetMapping struct {
+	Release		*Release
+	Asset		*Asset		// must be unique
+	Type			AssetType
+	Details		string		// primary assets of the given type MUST leave this unspecified
+}
