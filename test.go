fdda3dcf4ec8bde145ebc5bc528a1db1bbb0092b
Pietro Gagliardi <pietro10@mac.com>
2018-08-05 17:36:29 -0400

    And filled in more of the test database. Whee actual progress!

--- test.go
+++ test.go
@@ -22,6 +22,47 @@ func main() {
 		"Sega", "G-4002", db.Japan,
 		false, false, false,
 		mustParse("1988-10-29"), "https://sega.jp/fb/segahard/md/soft.html", fuzzytime.FuzzyTime{})
+	d.AddRelease(g,
+		"Space Harrier II", "", "",
+		"Sega", "", db.US,
+		false, false, false,
+		mustParse("1989"), "", fuzzytime.FuzzyTime{})
+
+	g, _ = d.AddGame(db.GameGame, db.MegaDrive)
+	d.AddRelease(g,
+		"スーパーサンダーブレード", "Super Thunder Blade", "https://sega.jp/fb/segahard/md/soft.html",
+		"Sega", "G-4003", db.Japan,
+		false, false, false,
+		mustParse("1988-10-29"), "https://sega.jp/fb/segahard/md/soft.html", fuzzytime.FuzzyTime{})
+
+	g, _ = d.AddGame(db.GameGame, db.MegaDrive)
+	d.AddRelease(g,
+		"獣王記", "Juuouki", "https://sega.jp/fb/segahard/md/soft.html",
+		"Sega", "G-4001", db.Japan,
+		false, false, false,
+		mustParse("1988-11-27"), "https://sega.jp/fb/segahard/md/soft.html", fuzzytime.FuzzyTime{})
+	d.AddRelease(g,
+		"Altered Beast", "", "",
+		"Sega", "", db.US,
+		false, false, false,
+		mustParse("1989"), "", fuzzytime.FuzzyTime{})
+	d.AddRelease(g,
+		"Altered Beast", "", "",
+		"Sega", "", db.Europe,
+		false, false, false,
+		mustParse("19??"), "", fuzzytime.FuzzyTime{})
+
+	g, _ = d.AddGame(db.GameGame, db.MegaDrive)
+	d.AddRelease(g,
+		"ワールドカップサッカー", "World Cup Soccer", "https://sega.jp/fb/segahard/md/soft.html",
+		"Sega", "G-4009", db.Japan,
+		false, false, false,
+		mustParse("1989-07-29"), "https://sega.jp/fb/segahard/md/soft.html", fuzzytime.FuzzyTime{})
+	d.AddRelease(g,
+		"World Championship Soccer", "", "",
+		"Sega", "", db.US,
+		false, false, false,
+		mustParse("1989"), "", fuzzytime.FuzzyTime{})
 
 	d.Write(os.Stdout)
 }
