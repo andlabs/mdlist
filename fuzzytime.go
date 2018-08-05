5ecb81e474496e84d11eaaed845ec6cab428f638
Pietro Gagliardi <pietro10@mac.com>
2018-08-04 23:43:26 -0400

    Made FuzzyTime more value-like and improved its tests, at the suggestion of various people in irc.freenode.net #go-nuts. Thanks to them!

--- fuzzytime.go
+++ fuzzytime.go
@@ -16,19 +16,17 @@ const (
 	Know2006
 	Know200601
 	Know20060102
-	// TODO include timestamp? split into FuzzyDate and FuzzyTime?
+	// TODO include timestamp?
 )
 
 // FuzzyTime is a fuzzy time.Time, which stores date values that
 // can have some parts be unknown; see the Accuracy type to see
-// how. The zero FuzzyTime is legal; it is the normalized form of
-// an invalid FuzzyTime.
+// how. The zero FuzzyTime is invalid and produces undefined
+// behavior (TODO).
 type FuzzyTime struct {
-	Time		time.Time
-	Accuracy	Accuracy
+	time		time.Time
+	accuracy	Accuracy
 }
-
-// TODO have a Date function?
 
 var TODOParseError = fmt.Errorf("parse error")
 
@@ -53,8 +51,8 @@ func Parse(s string) (FuzzyTime, error) {
 			return FuzzyTime{}, TODOParseError
 		}
 		return FuzzyTime{
-			Time:		time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC),
-			Accuracy:		Know20XX,
+			time:			time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC),
+			accuracy:		Know20XX,
 		}, nil
 	}
 	if !isDigit(s[2]) {
@@ -66,8 +64,8 @@ func Parse(s string) (FuzzyTime, error) {
 			return FuzzyTime{}, TODOParseError
 		}
 		return FuzzyTime{
-			Time:		time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC),
-			Accuracy:		Know200X,
+			time:			time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC),
+			accuracy:		Know200X,
 		}, nil
 	}
 	if !isDigit(s[3]) {
@@ -76,8 +74,8 @@ func Parse(s string) (FuzzyTime, error) {
 	year += int(s[3] - '0') * 1
 	if len(s) == 4 {
 		return FuzzyTime{
-			Time:		time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC),
-			Accuracy:		Know2006,
+			time:			time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC),
+			accuracy:		Know2006,
 		}, nil
 	}
 	// TODO accept 2006-?? as an alternate for 2006?
@@ -88,8 +86,8 @@ func Parse(s string) (FuzzyTime, error) {
 	month := time.Month(s[5] - '0') * 10 + time.Month(s[6] - '0')
 	if len(s) == 7 {
 		return FuzzyTime{
-			Time:		time.Date(year, month, 1, 0, 0, 0, 0, time.UTC),
-			Accuracy:		Know200601,
+			time:			time.Date(year, month, 1, 0, 0, 0, 0, time.UTC),
+			accuracy:		Know200601,
 		}, nil
 	}
 	if s[7] != '-' || !isDigit(s[8]) || !isDigit(s[9]) {
@@ -97,68 +95,26 @@ func Parse(s string) (FuzzyTime, error) {
 	}
 	day := int(s[8] - '0') * 10 + int(s[9] - '0')
 	return FuzzyTime{
-		Time:		time.Date(year, month, day, 0, 0, 0, 0, time.UTC),
-		Accuracy:		Know20060102,
+		time:			time.Date(year, month, day, 0, 0, 0, 0, time.UTC),
+		accuracy:		Know20060102,
 	}, nil
 }
 
-// IsValid returns whether f is valid. f is invalid if either the Time
-// stored within is zero or the Accuracy value is invalid.
-func (f FuzzyTime) IsValid() bool {
-	switch f.Accuracy {
-	case Know20XX:
-	case Know200X:
-	case Know2006:
-	case Know200601:
-	case Know20060102:
-		// do nothing
-	default:
-		return false
-	}
-	return !f.Time.IsZero()
+// Date returns the year, month, and day stored in f.
+// Digits of the year not provided by the accuracy of f are 0.
+// Months and days not provided by the accuracy of f are 1.
+func (f FuzzyTime) Date() (year int, month time.Month, day int) {
+	return f.time.Date()
 }
 
-// Normalized returns f with the Time field changed so that any
-// unknown field is either 0 or 1 (where appropriate) and so that
-// the time zone is UTC. This allows normalized times to be
-// compared and manipulated with standard time.Time functions
-// and methods. Calling Normalized on an invalid FuzzyTime
-// returns a zero FuzzyTime.
-func (f FuzzyTime) Normalized() FuzzyTime {
-	if !f.IsValid() {
-		return FuzzyTime{}
-	}
-	y, m, d := f.Time.Date()
-	switch f.Accuracy {
-	case Know20XX:
-		y /= 100
-		y *= 100
-		m = 1
-		d = 1
-	case Know200X:
-		y /= 10
-		y *= 10
-		m = 1
-		d = 1
-	case Know2006:
-		m = 1
-		d = 1
-	case Know200601:
-		d = 1
-	}
-	return FuzzyTime{
-		Time:		time.Date(y, m, d, 0, 0, 0, 0, time.UTC),
-		Accuracy:		f.Accuracy,
-	}
+// Accuracy returns the accuracy of f.
+func (f FuzzyTime) Accuracy() Accuracy {
+	return f.accuracy
 }
 
 func (f FuzzyTime) String() string {
-	f = f.Normalized()
-	if !f.IsValid() {
-		return "<invalid-fuzzy-time>"
-	}
-	y, m, d := f.Time.Date()
-	switch f.Accuracy {
+	y, m, d := f.time.Date()
+	switch f.accuracy {
 	case Know20XX:
 		return fmt.Sprintf("%d??", y / 100)
 	case Know200X:
--- fuzzytime_test.go
+++ fuzzytime_test.go
@@ -6,217 +6,245 @@ import (
 	"testing"
 )
 
-func TestParse(t *testing.T) {
-	// TODO
-}
-
-func TestIsValid(t *testing.T) {
+func TestPackage(t *testing.T) {
 	for _, tc := range []struct {
-		name	string
-		ft		FuzzyTime
-		valid		bool
+		str			string
+		invalid		bool
+		year			int
+		month		time.Month
+		day			int
+		accuracy		Accuracy
 	}{
 		{
-			name:	"valid 20?? time",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20XX,
-			},
-			valid:	true,
+			str:			"",
+			invalid:		true,
 		},
 		{
-			name:	"invalid 20?? time",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		Know20XX,
-			},
-			valid:	false,
+			str:			"abc",
+			invalid:		true,
 		},
 		{
-			name:	"valid 200? time",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know200X,
-			},
-			valid:	true,
+			str:			"????",
+			invalid:		true,
 		},
 		{
-			name:	"invalid 200? time",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		Know200X,
-			},
-			valid:	false,
+			str:			"19??",
+			year:			1900,
+			month:		1,
+			day:			1,
+			accuracy:		Know20XX,
 		},
 		{
-			name:	"valid 2006 time",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know2006,
-			},
-			valid:	true,
+			str:			"19XX",
+			invalid:		true,
 		},
 		{
-			name:	"invalid 2006 time",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		Know2006,
-			},
-			valid:	false,
+			str:			"199?",
+			year:			1990,
+			month:		1,
+			day:			1,
+			accuracy:		Know200X,
 		},
 		{
-			name:	"valid 2006-01 time",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know200601,
-			},
-			valid:	true,
+			str:			"1992",
+			year:			1992,
+			month:		1,
+			day:			1,
+			accuracy:		Know2006,
 		},
 		{
-			name:	"invalid 2006-01 time",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		Know200601,
-			},
-			valid:	false,
+			str:			"1992-08",		// month < 10
+			year:			1992,
+			month:		8,
+			day:			1,
+			accuracy:		Know200601,
 		},
 		{
-			name:	"valid 2006-01-02 time",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20060102,
-			},
-			valid:	true,
+			str:			"1992-8",
+			invalid:		true,
 		},
 		{
-			name:	"invalid 2006-01-02 time",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		Know20060102,
-			},
-			valid:	false,
+			str:			"1992/08",
+			invalid:		true,
 		},
 		{
-			name:	"invalid accuracy with valid time.Time",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		12345678,
-			},
-			valid:	false,
+			str:			"1992/8",
+			invalid:		true,
 		},
 		{
-			name:	"invalid accuracy with invalid time.Time",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		12345678,
-			},
-			valid:	false,
+			str:			"1992.08",
+			invalid:		true,
+		},
+		{
+			str:			"1992.8",
+			invalid:		true,
+		},
+		{
+			str:			"1992年08月",
+			invalid:		true,
+		},
+		{
+			str:			"1992年8月",
+			invalid:		true,
+		},
+		{
+			str:			"1992/08",
+			invalid:		true,
+		},
+		{
+			str:			"08/1992",
+			invalid:		true,
+		},
+		{
+			str:			"92/8",
+			invalid:		true,
+		},
+		{
+			str:			"8/92",
+			invalid:		true,
+		},
+		{
+			str:			"1992-12",
+			year:			1992,
+			month:		12,
+			day:			1,
+			accuracy:		Know200601,
+		},
+		{
+			str:			"1992-01-01",		// both < 10
+			year:			1992,
+			month:		1,
+			day:			1,
+			accuracy:		Know20060102,
+		},
+		{
+			str:			"1992-1-01",
+			invalid:		true,
+		},
+		{
+			str:			"1992-01-1",
+			invalid:		true,
+		},
+		{
+			str:			"1992-1-1",
+			invalid:		true,
+		},
+		{
+			str:			"1992/01/01",
+			invalid:		true,
+		},
+		{
+			str:			"1992/1/01",
+			invalid:		true,
+		},
+		{
+			str:			"1992/01/1",
+			invalid:		true,
+		},
+		{
+			str:			"1992/1/1",
+			invalid:		true,
+		},
+		{
+			str:			"1992.01.01",
+			invalid:		true,
+		},
+		{
+			str:			"1992.1.01",
+			invalid:		true,
+		},
+		{
+			str:			"1992.01.1",
+			invalid:		true,
+		},
+		{
+			str:			"1992.1.1",
+			invalid:		true,
+		},
+		{
+			str:			"1992年01月01日",
+			invalid:		true,
+		},
+		{
+			str:			"1992年1月01日",
+			invalid:		true,
+		},
+		{
+			str:			"1992年01月1日",
+			invalid:		true,
+		},
+		{
+			str:			"1992年1月1日",
+			invalid:		true,
+		},
+		{
+			str:			"1992/01/01",
+			invalid:		true,
+		},
+		{
+			str:			"01/01/1992",
+			invalid:		true,
+		},
+		{
+			str:			"92/1/1",
+			invalid:		true,
+		},
+		{
+			str:			"1/1/92",
+			invalid:		true,
+		},
+		{
+			str:			"1992-08-15",		// month < 10
+			year:			1992,
+			month:		8,
+			day:			15,
+			accuracy:		Know20060102,
+		},
+		{
+			str:			"1992-8-15",
+			invalid:		true,
+		},
+		{
+			str:			"1992-11-02",		// day < 10
+			year:			1992,
+			month:		11,
+			day:			2,
+			accuracy:		Know20060102,
+		},
+		{
+			str:			"1992-11-2",
+			invalid:		true,
+		},
+		{
+			str:			"1992-12-31",
+			year:			1992,
+			month:		12,
+			day:			31,
+			accuracy:		Know20060102,
 		},
 	} {
-		t.Run(tc.name, func(t *testing.T) {
-			gotValid := tc.ft.IsValid()
-			if gotValid != tc.valid {
-				t.Errorf("IsValid(%#v) failed: got %v, want %v", tc.ft, gotValid, tc.valid)
+		t.Run(tc.str, func(t *testing.T) {
+			ft, err := Parse(tc.str)
+			if tc.invalid {
+				if err == nil {
+					t.Fatalf("Parse(): got no error, want error")
+				}
+				return
 			}
-		})
-	}
-}
-
-func TestNormalized(t *testing.T) {
-	// TODO
-}
-
-func TestString(t *testing.T) {
-	for _, tc := range []struct {
-		name	string
-		ft		FuzzyTime
-		str		string
-	}{
-		{
-			name:	"invalid",
-			ft:		FuzzyTime{
-				Time:		time.Time{},
-				Accuracy:		Know20XX,
-			},
-			str:		"<invalid-fuzzy-time>",
-		},
-		{
-			name:	"20?? format",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20XX,
-			},
-			str:		"19??",
-		},
-		{
-			name:	"200? format",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know200X,
-			},
-			str:		"199?",
-		},
-		{
-			name:	"2006 format",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know2006,
-			},
-			str:		"1992",
-		},
-		{
-			name:	"2006-01 format",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 12, 31, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know200601,
-			},
-			str:		"1992-12",
-		},
-		{
-			name:	"2006-01 format, month < 10",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know200601,
-			},
-			str:		"1992-08",
-		},
-		{
-			name:	"2006-01-02 format",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 12, 31, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20060102,
-			},
-			str:		"1992-12-31",
-		},
-		{
-			name:	"2006-01-02 format, month < 10",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 8, 15, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20060102,
-			},
-			str:		"1992-08-15",
-		},
-		{
-			name:	"2006-01-02 format, day < 10",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 11, 2, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20060102,
-			},
-			str:		"1992-11-02",
-		},
-		{
-			name:	"2006-01-02 format, month < 10, day < 10",
-			ft:		FuzzyTime{
-				Time:		time.Date(1992, 1, 1, 0, 0, 0, 0, time.UTC),
-				Accuracy:		Know20060102,
-			},
-			str:		"1992-01-01",
-		},
-	} {
-		t.Run(tc.name, func(t *testing.T) {
-			got := tc.ft.String()
-			if got != tc.str {
-				t.Errorf("String(%#v) failed: got %q, want %q", tc.ft, got, tc.str)
+			if err != nil {
+				t.Fatalf("Parse(): got error %v, want no error", err)
+			}
+			y, m, d := ft.Date()
+			if y != tc.year || m != tc.month || d != tc.day {
+				t.Errorf("ft.Date(): got %d-%02d-%02d, want %d-%02d-%02d", y, m, d, tc.year, tc.month, tc.day)
+			}
+			a := ft.Accuracy()
+			if a != tc.accuracy {
+				t.Errorf("ft.Accuracy(): got %v, want %v", a, tc.accuracy)
+			}
+			str := ft.String()
+			if str != tc.str {
+				t.Errorf("ft.String(): got %q, want %q", str, tc.str)
 			}
 		})
 	}
